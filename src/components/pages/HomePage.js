312e4b5cfe3ec2d17ca33a2eed7d4f32e8ead3a2
thierry gravelaine <thierrygravelaine12@gmail.com>
2022-04-03 05:58:45 -0700

    authFront complete

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,83 +1,20 @@
-import React, {useState} from 'react'
-import { Link } from 'react-router-dom'
-import LoginPage from './LoginPage'
+import React from 'react'
 
-export default function HomePage() {
+export default function HomePage(props) {
 
-    const adminUser = {
-        email:"admin@gmail.com",
-        password : "admin123"
-      }
-    
-    
-      const [user, setUser] = useState({
-        name: "",
-        email: ""
-      }) 
-    
-      const [error, setError] = useState("");
-    
-      // function Login 
-    
-    
-    
-      const Login = details =>{
-        
-        console.log(details);
-    
-        if(details.email === adminUser.email && details.password === adminUser.password){
-          console.log("logged In");
-    
-          setUser({
-            name:details.name,
-            email:details.email
-    
-          });
-    
-        }else{
-          console.log("Details do not match ! ");
-          setError("Echec d'authentification. Try again or Sign up ! ");
-        }
-      }
-    
-      // function Logout
-    
-      const Logout = () =>{
-        setUser({
-          name:"",
-          email:""
-        });
-      }
+  const HandleLogout = ()=>{
+
+    props.history.push('/login');
+  }
 
     return (
        
-        <div className="text-center">
+        <div className="HomePage">
+          Welcome User ! 
+          <br></br>
 
-        {
-            (user.email!=="") ? 
-            
-            (
-                <div className="main-title home-page-title">
-                    <h1>welcome to our app</h1>
-                    <Link to="/">
-                        <button className="primary-button" onClick={Logout}>Log out</button>
-                    </Link>
-                </div>
-
-            ) 
-            
-            : 
-            
-            
-            (
-                <LoginPage Login={Login} error={error}/>
-            )
-
-
-        }
-        
+          <button onClick={HandleLogout}>Logout</button>
         </div>
-
 
     )
 }
--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,25 +1,15 @@
+import axios from 'axios';
 import React, {useState} from 'react'
 import { Link }  from 'react-router-dom'
 
 import '../../App.css'
 
 
-export default function SignInPage() {
+export default function SignInPage(props) {
 
 
     
-    const adminUser = {
-        email:"admin@gmail.com",
-        password : "admin123"
-    }
-    
-    
-      const [user, setUser] = useState({
-        name: "",
-        email: ""
-      }) 
-    
-    const [error, setError] = useState("");
+   
 
     // Hooks Pour préparer le modèle de données à recevoir 
     
@@ -31,82 +21,49 @@ export default function SignInPage() {
     // La fonction de soumission du formulaire
 
     const submitHandler = e =>{
-        e.preventDefault();
-        Login(details);
+
+      axios.post("http://localhost:3333/auth/local/signin", {
+        email : details.email,
+        password : details.password
+      })
+      .then(reponse =>{
+        console.log("response =====>", reponse);
+      })
+      .catch(error =>{
+        console.error("error =====>", error);
+      });
+
+        /* e.preventDefault();
+        props.history.push('/home') */
     }
 
-    const Login = details =>{
-        
-        console.log(details);
     
-        if(details.email === adminUser.email && details.password === adminUser.password){
-          console.log("logged In");
-    
-          setUser({
-            name:details.name,
-            email:details.email
-    
-          });
-
-          setError("");
-
-          
-    
-        }else{
-          console.log("Details do not match ! ");
-          setError("Echec d'authentification. Try again or Sign up ! ");
-        }
-      }
-
       // redirection funciton
 
     return (
         <div className="text-center m-5-auto">
 
             
-            {
-            
-            (error !=="") ?
-
-            (<div className="error">{error}</div>) 
-            
-            : 
-            
-            ""
           
-          
-          }
-
-          {
-              (user.email!=="")?
-              (
-                <div className="main-title home-page-title">
-                    <h1>welcome to our app</h1>
-                    <Link to="/">
-                        <button className="primary-button">Log out</button>
-                    </Link>
-                </div>
-              ):
-              (
                 
                 <form onSubmit={submitHandler}>
                     <h2>Sign in to us</h2>
                     <p>
                         <label>Username or email address</label><br/>
-                        <input type="text" name="email" onChange={ e=> setDetails({...details, email: e.target.value})} value={details.email} required />
+                        <input type="text" name="email" onChange={ e=> setDetails({...details, email: e.target.value})} value={details.email}  />
                     </p>
                     <p>
                         <label>Password</label>
                         <Link to="/forget-password"><label className="right-label">Forget password?</label></Link>
                         <br/>
-                        <input type="password" name="password" onChange={ e=> setDetails({...details, password: e.target.value})} value={details.password} required />
+                        <input type="password" name="password" onChange={ e=> setDetails({...details, password: e.target.value})} value={details.password}  />
                     </p>
                     <p>
                         <button id="sub_btn" type="submit">Login</button>
                     </p>
                 </form>
-              )
-          }
+              
+          
             
             <footer>
                 <p>First time? <Link to="/register">Create an account</Link>.</p>
